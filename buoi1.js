df8867b40e16fbf01c3bc87737eaa7e1353289e2
phatdroid94 <phatdroid94@gmail.com>
2021-01-02 20:08:58 +0700

    kieu dữ liệu

--- buoi1.js
+++ buoi1.js
@@ -1,5 +1,22 @@
 // 1 : Biến
-var a = "Pham Tan Phat" 
-const name = "Nguyen Van Teo"
+// comment code : ctrl + /
+// var a = "Pham Tan Phat" 
+// const name = "Nguyen Van Teo"
 
 // mutable (thay đổi trạng thái) vs immutable(Không thay đổi)
+
+// 2 : Kieu du lieu
+// primities
+
+// object (reference)
+
+// null : Trỏ tới tham chiếu tới đối tượng nhưng vùng nhớ không có hoặc không tồn tại
+// undefined
+
+// TH1 : Khai báo biến bằng từ khóa var nhưng truyền giá trị
+// console.log(a)
+// var a
+
+// npm i -g nodemon : Thư viện nodemon
+
+
