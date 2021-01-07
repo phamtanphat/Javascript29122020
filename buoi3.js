32ab96140a1d2aa029a557de1f9bd49461525b21
phatdroid94 <phatdroid94@gmail.com>
2021-01-07 20:17:46 +0700

    bai tap reduce

--- buoi3.js
+++ buoi3.js
@@ -6,23 +6,47 @@
 // }, 0 )
 
 // console.log(result)
+
 // Trả về số lượng người đã vote
-function totalVotes(arr) {
+// function totalVotes(arr) {
+//     const resultTotalVote = arr.reduce(function(accumulator , currentValue){
+//         if(currentValue.voted){
+//             accumulator += 1
+//         }
+//         return accumulator
+//     },0)
+//     console.log(resultTotalVote)
+// }
 
+// var voters = [
+//   {name:'Bob' , age: 30, voted: true},
+//   {name:'Jake' , age: 32, voted: true},
+//   {name:'Kate' , age: 25, voted: false},
+//   {name:'Sam' , age: 20, voted: false},
+//   {name:'Phil' , age: 21, voted: true},
+//   {name:'Ed' , age:55, voted:true},
+//   {name:'Tami' , age: 54, voted:true},
+//   {name: 'Mary', age: 31, voted: false},
+//   {name: 'Becky', age: 43, voted: false},
+//   {name: 'Joey', age: 41, voted: true},
+//   {name: 'Jeff', age: 30, voted: true},
+//   {name: 'Zack', age: 19, voted: false}
+// ];
+// totalVotes(voters) // 7
+
+function countOccurrences(arr) {
+    const object = arr.reduce(function(accumulator , currentValue){
+        if (currentValue in accumulator){
+            accumulator[currentValue]++
+        }else{
+            accumulator[currentValue] = 1
+        }
+        return accumulator
+    },{})
+    console.log(object)  
 }
+countOccurrences(["a", "b", "c", "b", "a", "a"]);
 
-var voters = [
-  {name:'Bob' , age: 30, voted: true},
-  {name:'Jake' , age: 32, voted: true},
-  {name:'Kate' , age: 25, voted: false},
-  {name:'Sam' , age: 20, voted: false},
-  {name:'Phil' , age: 21, voted: true},
-  {name:'Ed' , age:55, voted:true},
-  {name:'Tami' , age: 54, voted:true},
-  {name: 'Mary', age: 31, voted: false},
-  {name: 'Becky', age: 43, voted: false},
-  {name: 'Joey', age: 41, voted: true},
-  {name: 'Jeff', age: 30, voted: true},
-  {name: 'Zack', age: 19, voted: false}
-];
-totalVotes(voters) // 7
+// {a : 3 , b : 2 , c : 1}
+
+
