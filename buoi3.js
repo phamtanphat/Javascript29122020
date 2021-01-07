f9f5a89b566c4af2160b26a35a09aed45bc760db
phatdroid94 <phatdroid94@gmail.com>
2021-01-07 20:01:21 +0700

    reduce

---
+++ buoi3.js
@@ -0,0 +1,28 @@
+// 13 : Ham reduce
+// const arrNumbers = [1,2,3,4,5]
+
+// const result = arrNumbers.reduce(function(accumulator , currentValue){
+//     return accumulator + currentValue
+// }, 0 )
+
+// console.log(result)
+// Trả về số lượng người đã vote
+function totalVotes(arr) {
+
+}
+
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+totalVotes(voters) // 7
